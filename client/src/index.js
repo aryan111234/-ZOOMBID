8c1998bec6323d2cdbd2b4305c1ee9e4bd414511
Aryan <aryandhakal92@gmail.com>
2024-02-27 13:25:21 +0545

    Updating

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ConfigProvider } from "antd";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        components: {
+          Button: {
+            colorPrimary: "#405138",
+            colorPrimaryHover: "#405138",
+            borderRadius: "0px",
+          },
+        },
+      }}
+    ></ConfigProvider>
     <App />
   </React.StrictMode>
 );
